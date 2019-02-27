73103b8a01cfde35b12b1e1b1c87a8eb1d61c115
YDZ <ydz627@gmail.com>
2019-02-27 10:42:09 +0800

    添加 problem 23 解法

---
+++ Algorithms/23.Merge k Sorted Lists/Merge k Sorted Lists.go
@@ -0,0 +1,38 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	length := len(lists)
+	if length < 1 {
+		return nil
+	}
+	if length == 1 {
+		return lists[0]
+	}
+	num := length / 2
+	left := mergeKLists(lists[:num])
+	right := mergeKLists(lists[num:])
+	return mergeTwoLists1(left, right)
+}
+
+func mergeTwoLists1(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists1(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists1(l1, l2.Next)
+		return l2
+	}
+}
