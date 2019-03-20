ab1355b13a65c7101ea7a9b8e1ec8576ebddb942
YDZ <ydz627@gmail.com>
2019-03-20 09:05:16 +0800

    增加 problem 142

---
+++ Algorithms/142. Linked List Cycle II/142. Linked List Cycle II.go
@@ -0,0 +1,37 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	isCycle, slow := hasCycle_(head)
+	if !isCycle {
+		return nil
+	}
+	fast := head
+	for fast != slow {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return fast
+}
+
+func hasCycle_(head *ListNode) (bool, *ListNode) {
+	fast := head
+	slow := head
+	for slow != nil && fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true, slow
+		}
+	}
+	return false, nil
+}
---
+++ Algorithms/142. Linked List Cycle II/142. Linked List Cycle II_test.go
@@ -0,0 +1,6 @@
+package leetcode
+
+import "testing"
+
+func Test_Problem142(t *testing.T) {
+}
