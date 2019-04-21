ae03b00bac2e6c89379aff0495b9a8e95e72013d
YDZ <ydz627@gmail.com>
2019-04-21 22:05:03 +0800

    添加 problem 80

---
+++ Algorithms/80. Remove Duplicates from Sorted Array II/80. Remove Duplicates from Sorted Array II.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func removeDuplicates_80(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	last, finder, startFinder := 0, 0, -1
+	for last < len(nums)-1 {
+		startFinder = -1
+		for nums[finder] == nums[last] {
+			if startFinder == -1 || startFinder > finder {
+				startFinder = finder
+			}
+			if finder == len(nums)-1 {
+				break
+			}
+			finder++
+		}
+		if finder-startFinder >= 2 && nums[finder-1] == nums[last] && nums[finder] != nums[last] {
+			nums[last+1] = nums[finder-1]
+			nums[last+2] = nums[finder]
+			last += 2
+		} else {
+			nums[last+1] = nums[finder]
+			last += 1
+		}
+		if finder == len(nums)-1 {
+			if nums[finder] != nums[last-1] {
+				nums[last] = nums[finder]
+			}
+			return last + 1
+		}
+	}
+	return last + 1
+}
---
+++ Algorithms/80. Remove Duplicates from Sorted Array II/80. Remove Duplicates from Sorted Array II_test.go
@@ -0,0 +1,67 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question80 struct {
+	para80
+	ans80
+}
+
+// para 是参数
+// one 代表第一个参数
+type para80 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans80 struct {
+	one int
+}
+
+func Test_Problem80(t *testing.T) {
+
+	qs := []question80{
+
+		question80{
+			para80{[]int{1, 1, 2}},
+			ans80{3},
+		},
+
+		question80{
+			para80{[]int{0, 0, 1, 1, 1, 1, 2, 3, 4, 4}},
+			ans80{8},
+		},
+
+		question80{
+			para80{[]int{0, 0, 0, 0, 0}},
+			ans80{2},
+		},
+
+		question80{
+			para80{[]int{1}},
+			ans80{1},
+		},
+
+		question80{
+			para80{[]int{0, 0, 1, 1, 1, 1, 2, 3, 3}},
+			ans80{7},
+		},
+
+		question80{
+			para80{[]int{1, 1, 1, 1, 2, 2, 3}},
+			ans80{5},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 80------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans80, q.para80
+		fmt.Printf("【input】:%v    【output】:%v\n", p.one, removeDuplicates_80(p.one))
+	}
+	fmt.Printf("\n\n\n")
+}
