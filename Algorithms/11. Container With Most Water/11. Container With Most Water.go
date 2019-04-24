57ca07c8559b38ca2ea509fe3b40d9a70920ea82
YDZ <ydz627@gmail.com>
2019-04-24 08:20:22 +0800

    添加 problem 11

---
+++ Algorithms/11. Container With Most Water/11. Container With Most Water.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func maxArea(height []int) int {
+	max, start, end := 0, 0, len(height)-1
+	for start < end {
+		width := end - start
+		high := 0
+		if height[start] < height[end] {
+			high = height[start]
+			start++
+		} else {
+			high = height[end]
+			end--
+		}
+
+		temp := width * high
+		if temp > max {
+			max = temp
+		}
+	}
+	return max
+}
---
+++ Algorithms/11. Container With Most Water/11. Container With Most Water_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question11 struct {
+	para11
+	ans11
+}
+
+// para 是参数
+// one 代表第一个参数
+type para11 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans11 struct {
+	one int
+}
+
+func Test_Problem11(t *testing.T) {
+
+	qs := []question11{
+
+		question11{
+			para11{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}},
+			ans11{49},
+		},
+
+		question11{
+			para11{[]int{1, 1}},
+			ans11{1},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 11------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans11, q.para11
+		fmt.Printf("【input】:%v    【output】:%v\n", p.one, maxArea(p.one))
+	}
+	fmt.Printf("\n\n\n")
+}
