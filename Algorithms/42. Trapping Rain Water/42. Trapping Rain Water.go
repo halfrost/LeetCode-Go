816c11762c3c13b887c367b67b01c325e4a486d4
YDZ <ydz627@gmail.com>
2019-06-07 23:21:13 +0800

    添加 problem 42

---
+++ Algorithms/42. Trapping Rain Water/42. Trapping Rain Water.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func trap(height []int) int {
+	res, left, right, maxLeft, maxRight := 0, 0, len(height)-1, 0, 0
+	for left <= right {
+		if height[left] <= height[right] {
+			if height[left] > maxLeft {
+				maxLeft = height[left]
+			} else {
+				res += maxLeft - height[left]
+			}
+			left++
+		} else {
+			if height[right] >= maxRight {
+				maxRight = height[right]
+			} else {
+				res += maxRight - height[right]
+			}
+			right--
+		}
+	}
+	return res
+}
---
+++ Algorithms/42. Trapping Rain Water/42. Trapping Rain Water_test.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question42 struct {
+	para42
+	ans42
+}
+
+// para 是参数
+// one 代表第一个参数
+type para42 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans42 struct {
+	one int
+}
+
+func Test_Problem42(t *testing.T) {
+
+	qs := []question42{
+
+		question42{
+			para42{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}},
+			ans42{6},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 42------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans42, q.para42
+		fmt.Printf("【input】:%v       【output】:%v\n", p, trap(p.one))
+	}
+	fmt.Printf("\n\n\n")
+}
