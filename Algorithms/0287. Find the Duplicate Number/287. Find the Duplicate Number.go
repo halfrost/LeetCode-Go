09de48730927672a74dfed1bd246c448bdb99ed0
YDZ <ydz627@gmail.com>
2019-09-30 18:00:40 +0800

    更新 problem 287

--- Algorithms/0287. Find the Duplicate Number/287. Find the Duplicate Number.go
+++ Algorithms/0287. Find the Duplicate Number/287. Find the Duplicate Number.go
@@ -2,7 +2,7 @@ package leetcode
 
 import "sort"
 
-// 解法一
+// 解法一 快慢指针
 func findDuplicate(nums []int) int {
 	slow := nums[0]
 	fast := nums[nums[0]]
@@ -18,8 +18,27 @@ func findDuplicate(nums []int) int {
 	return walker
 }
 
-// 解法二
+// 解法二 二分搜索
 func findDuplicate1(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid, count := low+(high-low)>>1, 0
+		for _, num := range nums {
+			if num <= mid {
+				count++
+			}
+		}
+		if count > mid {
+			high = mid
+		} else {
+			low = mid + 1
+		}
+	}
+	return low
+}
+
+// 解法三
+func findDuplicate2(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
