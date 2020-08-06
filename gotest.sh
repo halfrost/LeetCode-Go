ef3da5d363909c05150b9f0099cbabfa611ab9d0
YDZ <ydz627@gmail.com>
2020-08-06 22:40:35 +0800

    add travis-ci

---
+++ gotest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./Algorithms/... | grep -v vendor); do
+    echo $d
+    go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
