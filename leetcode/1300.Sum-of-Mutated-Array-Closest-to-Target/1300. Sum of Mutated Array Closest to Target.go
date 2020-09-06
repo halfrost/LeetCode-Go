d1d2ade86814387c543e5fd48d802cd87e30b1b5
KeroJin <7380114+YongjinHuang@users.noreply.github.com>
2020-09-07 00:17:05 +0800

    Update 1300. Sum of Mutated Array Closest to Target.go
    输入用例是 arr = [2, 3, 5], target = 11

--- leetcode/1300.Sum-of-Mutated-Array-Closest-to-Target/1300. Sum of Mutated Array Closest to Target.go
+++ leetcode/1300.Sum-of-Mutated-Array-Closest-to-Target/1300. Sum of Mutated Array Closest to Target.go
@@ -10,6 +10,16 @@ func findBestValue(arr []int, target int) int {
 			high = mid
 		}
 	}
+	// 假设输入用例是 arr = [2, 3, 5], target = 11，直接返回数组最大值即可
+	if r == 100000 {
+        	res := 0
+        	for _, num := range arr {
+        		if res < num {
+                		res = num
+            		}
+        	}
+        	return res
+    	}	
 	// 比较阈值线分别定在 left - 1 和 left 的时候与 target 的接近程度
 	sum1, sum2 := calculateSum(arr, low-1), calculateSum(arr, low)
 	if target-sum1 <= sum2-target {
