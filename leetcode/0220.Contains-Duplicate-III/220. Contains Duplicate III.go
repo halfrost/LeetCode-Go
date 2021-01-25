ebebf5c1a82e951f932e465ef80c2bf3f3532088
YDZ <ydz627@gmail.com>
2021-01-25 16:24:12 +0800

    Modify solution 220

--- leetcode/0220.Contains-Duplicate-III/220. Contains Duplicate III.go
+++ leetcode/0220.Contains-Duplicate-III/220. Contains Duplicate III.go
@@ -1,49 +1,36 @@
 package leetcode
 
-import "sort"
-
-// 解法一 排序 + 滑动窗口
+// 解法一 桶排序
 func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
-	if len(nums) < 2 {
+	if k <= 0 || t < 0 || len(nums) < 2 {
 		return false
 	}
-	elemList := make([]*elem, len(nums))
-	for i, num := range nums {
-		elemList[i] = &elem{num, i}
-	}
-	sort.SliceStable(elemList, func(i, j int) bool {
-		if elemList[i].val != elemList[j].val {
-			return elemList[i].val < elemList[j].val
+	buckets := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		// Get the ID of the bucket from element value nums[i] and bucket width t + 1
+		key := nums[i] / (t + 1)
+		// -7/9 = 0, but need -7/9 = -1
+		if nums[i] < 0 {
+			key--
 		}
-		return elemList[i].idx < elemList[j].idx
-	})
-	i, j := 0, 1
-	for j < len(elemList) {
-		if elemList[j].val-elemList[i].val <= t {
-			if abs(elemList[j].idx-elemList[i].idx) <= k {
-				return true
-			}
-			j++
-		} else {
-			i++
-			if j <= i {
-				j++
-			}
+		if _, ok := buckets[key]; ok {
+			return true
 		}
+		// check the lower bucket, and have to check the value too
+		if v, ok := buckets[key-1]; ok && nums[i]-v <= t {
+			return true
+		}
+		// check the upper bucket, and have to check the value too
+		if v, ok := buckets[key+1]; ok && v-nums[i] <= t {
+			return true
+		}
+		// maintain k size of window
+		if len(buckets) >= k {
+			delete(buckets, nums[i-k]/(t+1))
+		}
+		buckets[key] = nums[i]
 	}
 	return false
-}
-
-func abs(a int) int {
-	if a > 0 {
-		return a
-	}
-	return -a
-}
-
-type elem struct {
-	val int
-	idx int
 }
 
 // 解法二 滑动窗口 + 剪枝
@@ -65,4 +52,11 @@ func containsNearbyAlmostDuplicate1(nums []int, k int, t int) bool {
 		}
 	}
 	return false
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
 }
