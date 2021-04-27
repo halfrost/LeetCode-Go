0a614a0e7971f3081f5b5f0de2119161993b5ac9
novahe <heqianfly@gmail.com>
2021-04-27 12:53:02 +0800

    fix 167: clean up redundant code

--- leetcode/0167.Two-Sum-II---Input-array-is-sorted/167. Two Sum II - Input array is sorted.go
+++ leetcode/0167.Two-Sum-II---Input-array-is-sorted/167. Two Sum II - Input array is sorted.go
@@ -6,7 +6,8 @@ func twoSum167(numbers []int, target int) []int {
 	for i < j {
 		if numbers[i]+numbers[j] == target {
 			return []int{i + 1, j + 1}
-		} else if numbers[i]+numbers[j] < target {
+		}
+		if numbers[i]+numbers[j] < target {
 			i++
 		} else {
 			j--
@@ -20,8 +21,8 @@ func twoSum167_1(numbers []int, target int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(numbers); i++ {
 		another := target - numbers[i]
-		if _, ok := m[another]; ok {
-			return []int{m[another] + 1, i + 1}
+		if idx, ok := m[another]; ok {
+			return []int{idx + 1, i + 1}
 		}
 		m[numbers[i]] = i
 	}
