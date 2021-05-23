58f6f7fedbdb3b3c63c04a2f391db340e1825e70
novahe <heqianfly@gmail.com>
2021-05-23 13:14:31 +0800

    clean up 62

--- leetcode/0062.Unique-Paths/62. Unique Paths.go
+++ leetcode/0062.Unique-Paths/62. Unique Paths.go
@@ -5,14 +5,12 @@ func uniquePaths(m int, n int) int {
 	for i := 0; i < n; i++ {
 		dp[i] = make([]int, m)
 	}
-	for i := 0; i < m; i++ {
-		dp[0][i] = 1
-	}
 	for i := 0; i < n; i++ {
-		dp[i][0] = 1
-	}
-	for i := 1; i < n; i++ {
-		for j := 1; j < m; j++ {
+		for j := 0; j < m; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+				continue
+			}
 			dp[i][j] = dp[i-1][j] + dp[i][j-1]
 		}
 	}
