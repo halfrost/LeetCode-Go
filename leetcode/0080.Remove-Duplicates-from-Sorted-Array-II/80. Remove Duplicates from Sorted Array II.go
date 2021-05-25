7a3fe1fed2d39b69ecc214fa10f5d5e4d28180a2
novahe <heqianfly@gmail.com>
2021-05-25 22:21:37 +0800

    add 80 solution explain

--- leetcode/0080.Remove-Duplicates-from-Sorted-Array-II/80. Remove Duplicates from Sorted Array II.go
+++ leetcode/0080.Remove-Duplicates-from-Sorted-Array-II/80. Remove Duplicates from Sorted Array II.go
@@ -2,8 +2,8 @@ package leetcode
 
 func removeDuplicates(nums []int) int {
 	slow := 0
-	for i, v := range nums {
-		if i < 2 || nums[slow-2] != v {
+	for fast, v := range nums {
+		if fast < 2 || nums[slow-2] != v {
 			nums[slow] = v
 			slow++
 		}
